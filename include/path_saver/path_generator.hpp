5e3e76a26c45fc006702d57cb47df55a56ff0220
JunsoMin <minjs4562@gmail.com>
2024-07-10 06:03:46 +0000

    feat: path_generator

--- include/path_saver/path_generator.hpp
+++ include/path_saver/path_generator.hpp
@@ -7,6 +7,7 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
+#include "tf2/buffer_core.h"
 
 #include <fstream>
 
@@ -14,7 +15,13 @@ class Path_Generator : public rclcpp::Node
 {
 public:
   Path_Generator();
+
 private:
-  // rclcpp::Subscription<> subscriber_;
+  void timer_cb();
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  tf2_ros::Buffer buffer_;
+  tf2_ros::TransformListener listener_;
+  std::ofstream file_;
 };
 #endif  // PATH_SAVER__PATH_GENERATOR_HPP_
--- src/path_generator.cpp
+++ src/path_generator.cpp
@@ -1,7 +1,36 @@
 #include "path_saver/path_generator.hpp"
 
-Path_Generator::Path_Generator() : Node("path_generator")
+using namespace std::chrono_literals;
+
+Path_Generator::Path_Generator() 
+: Node("path_generator"), buffer_(this->get_clock()),listener_(buffer_)
 {
+  timer_ = this -> create_wall_timer(100ms, std::bind(&Path_Generator::timer_cb,this));
+  file_.open("path/globalpath.csv");
+  file_<<"x,y,z \n";
+
+  RCLCPP_INFO(this->get_logger(), "**** path_generator initalized!! ****");
+}
+
+void Path_Generator::timer_cb(){
+  try
+  {
+    geometry_msgs::msg::TransformStamped ts;
+    ts = buffer_.lookupTransform("map","base_link",tf2::TimePointZero);
+    double p_x = ts.transform.translation.x;
+    double p_y = ts.transform.translation.y;
+    double p_z = ts.transform.translation.z;
+
+    if (file_.is_open())
+    {
+      file_<<p_x << ','<< p_y << ',' << p_z << '\n';
+    }
+    
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
   
 }
 
